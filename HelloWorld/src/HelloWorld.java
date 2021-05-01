77241547c3502934b568537a094989b4bf5677c9
kaychoe <kaychoe@naver.com>
2021-05-01 19:34:18 +0900

    Change Encoding format to UTF-8

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("Simple test for version control");
+        System.out.println("한글 인코딩은 잘 되나?");
     }
 }
